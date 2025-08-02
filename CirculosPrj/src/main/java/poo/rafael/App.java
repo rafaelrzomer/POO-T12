6505fbc032be64816face3ce58506723d816c02a
rafaelrzomer <rafaelrzomer@hotmail.com>
2025-08-02 22:45:36 +0000

    codigo atualizado

--- CirculosPrj/src/main/java/poo/rafael/App.java
+++ CirculosPrj/src/main/java/poo/rafael/App.java
@@ -2,6 +2,27 @@ package poo.rafael;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world! Projeto CirculosPrj criado com sucesso.");
+        
+        Circulo c1 = new Circulo(2,3,5);
+        Circulo c2 = new Circulo();
+
+        System.out.println("Circulo 1: \n" + c1 + "\n");
+        System.out.println("Circulo 2: \n" + c2 + "\n");
+
+        c1.Mover(4,2);
+        System.out.println("Circulo 1 movido:\n" + c1 + "\n");
+
+        c1.Zoom(5);
+        System.out.println("Circulo 1 com Zoom:\n" + c1 + "\n");
+
+        Circulo [] circulos = new Circulo[3];
+        circulos[0] = new Circulo(1,2,3);
+        circulos[1] = new Circulo(4,3,2);
+        circulos[2] = new Circulo(5,5,3);
+
+        for (int i = 0; i< circulos.length; i++){
+            System.out.println(circulos[i]);
+        }
+
     }
 }
---
+++ CirculosPrj/src/main/java/poo/rafael/Circulo.java
@@ -0,0 +1,39 @@
+package poo.rafael;
+
+public class Circulo {
+
+    private double x;
+    private double y;
+    private double raio;
+
+public Circulo (double x, double y, double raio) {
+
+    this.x = x;
+    this.y = y;
+    this.raio = raio;
+
+}
+
+public Circulo() {
+    this.x = 0;
+    this.y = 0;
+    this.raio = 1;
+
+}
+
+@Override
+public String toString(){
+return String.format ("Centro: %.2f, %.2f \nRaio: %.2f", x,y,raio);
+}
+
+public void Mover(double moveX, double moveY){
+        this.x = moveX;
+        this.y = moveY;
+    }
+
+    public void Zoom(double zoom){
+        this.raio = raio*zoom;
+    }
+
+}
+
