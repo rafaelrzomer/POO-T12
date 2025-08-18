c6238c8449d44ceaa84ff400e2388b81bed7f55c
rafaelrzomer <rafaelrzomer@hotmail.com>
2025-08-18 21:21:04 +0000

    aula18.08

--- VendasPrj/src/main/java/rafael/poo/App.java
+++ VendasPrj/src/main/java/rafael/poo/App.java
@@ -4,27 +4,34 @@ package rafael.poo;
 public class App {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto(15321,"mesa",1000);
-        System.out.println(p1.toString());
-        Cliente c1 = new Cliente("Rafael","123.456.789-00");
-        System.out.println(c1.toString());
-
         CadastroProdutos cadProd = new CadastroProdutos();
-
         cadProd.cadastrar(1231,"Lapis",2.99);
+        cadProd.cadastrar(1232,"Caneta",3.99);
+        cadProd.cadastrar(1233, "Borracha", 0.50);
+        cadProd.cadastrar(1234, "Tesoura", 6.50);
+        cadProd.cadastrar(1235, "Caderno", 19.90);
 
         System.out.println("Produtos cadastrados: ");
         System.out.println(cadProd.relatorio());
 
-        Produto Prod = cadProd.pesquisa(1231);
-        if (Prod == null)
-        System.out.println("O produto tal nao existe.");
+        Produto prod = cadProd.pesquisa(1231);
+        if (prod == null)
+        System.out.printf("O código digitado nao existe.");
         else
-        System.out.println("Achou " + Prod);
+        System.out.println("Achou: \n" + prod);
 
 
-        ItemVenda iv1 = new ItemVenda(10, Prod);
+        ItemVenda iv1 = new ItemVenda(10, prod);
         System.out.println("ItemVenda: " + iv1.toString());
+
+        Venda venda1 = new Venda();
+        venda1.inserir(10,prod);
+        prod = cadProd.pesquisa(1232);
+        venda1.inserir(5, prod);
+        prod = cadProd.pesquisa(1233);
+        venda1.inserir(prod);
+
+        System.out.println(venda1.fecharVenda());
 
     }
 
--- VendasPrj/src/main/java/rafael/poo/Venda.java
+++ VendasPrj/src/main/java/rafael/poo/Venda.java
@@ -9,30 +9,35 @@ public class Venda {
         itens = new ArrayList<>();
     }
 
-    public void Inserir(Produto prod){
+    public void inserir(Produto prod){
     ItemVenda iv = new ItemVenda(1, prod);
     itens.add(iv);
-
     }
 
-    public void Inserir(int quantidade, Produto prod){
-    ItemVenda iv = new ItemVenda(int quantidade, Produto prod);
-    itens.add(iv);
-
-        
+    public void inserir(int quantidade, Produto prod){
+    ItemVenda iv = new ItemVenda(quantidade, prod);
+    itens.add(iv); 
     }
     
-    public Double valorTotal(){
+    public double valorTotal(){
+    double total = 0;
 
     for (ItemVenda item : itens) {
         total = total + item.getValor();
-    }
+        }
     return total;
     }
 
-
     public String fecharVenda(){
-        Stringbuilder notaF = New StringBuilder("\nPoo Vendas \t Nota fiscal")
+        StringBuilder notaF = new StringBuilder ("\nVenda finalizada. \n Resumo:\n");
+
+        for (ItemVenda item : itens){
+            notaF.append("\t");
+            notaF.append(item.toString());
+            notaF.append("\n");
+        }
+        return notaF.toString();
+
     }
 
 }
